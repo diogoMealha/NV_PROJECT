31c6f5c59c48025dcd58f4d4fde2e54e4fa52e12
pablo <pablo@pablos-air.dtuguest.clients.local>
2025-06-12 14:22:58 +0200

    first IMAGEEEEEE

--- test_pablo.py
+++ test_pablo.py
@@ -1,15 +1,40 @@
+import pickle
+from ffpyplayer.pic import Image as FFPImage
+import numpy as np
+import matplotlib.pyplot as plt
 
-from thorcam.camera import ThorCam
-from thorcam.camera import ThorCamClient
+
+def load_frame_from_pickle(path: str) -> 'ffpyplayer.pic.Image':
+    with open(path, 'rb') as f:
+        data = pickle.load(f)
+
+    # 3) rebuild the ffpyplayer.pic.Image
+    img = FFPImage(
+        data['planes'],
+        pix_fmt   = data['pix_fmt'],
+        size      = data['size'],
+
+    )
+    return img
+
+# --- usage ---pip
+
+# later when you need it back:
+img2 = load_frame_from_pickle('obj2.bn')
 
 
-def main():
-    ThorCamClient.thor_bin_path=r"C:\Program Files\Thorlabs\Scientific Imaging\ThorCam"
+pix_fmt = img2.get_pixel_format()
+print(pix_fmt)
 
-    cam = ThorCam()
-    cam.start_cam_process()
-    cam.refresh_cameras()
-  
+# get raw bytes and dimensions
+buf = img2.to_bytearray()[0]
+width, height = img2.get_size()
 
-if __name__ == "__main__":
-    main()
+# interpret as little-endian 16-bit grayscale
+data = np.frombuffer(buf, dtype='<u2')   # uint16, little-endian
+arr = data.reshape((height, width))
+
+# display
+plt.imshow(arr, cmap='gray', vmin=arr.min(), vmax=arr.max())
+plt.axis('off')
+plt.show()
