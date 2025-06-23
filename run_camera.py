1a95cbf0d6d246eb6b902d41d4ee0abda2a9bb05
Juff-hash <lucasclausendk@gmail.com>
2025-06-23 14:20:05 +0200

    some updates

--- run_camera.py
+++ run_camera.py
@@ -9,10 +9,16 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import queue
 
-CAM_IMG_SIZE = (2448, 2048)  # 
-
+CAM_IMG_SIZE = (2048, 2448)  # 
+MAG_BASELINE = np.zeros(CAM_IMG_SIZE)
+#global IMAGE_LIST
+IMAGE_LIST = []
 
 class MyThorCam(ThorCam):
+
+
+    # def __init__(self):
+    #     global MAG_BASELINE
     def received_camera_response(self, msg, value):
         super(MyThorCam, self).received_camera_response(msg, value)
         if msg == 'image':
@@ -24,10 +30,32 @@ class MyThorCam(ThorCam):
         
         if count > 500:
             exit()
-            
-        if count == 10:
-            np.savez("images/with_mag.npz", image_data)
-            exit()
+        
+        if count < 100:
+            global MAG_BASELINE
+            MAG_BASELINE = image_data.copy()
+        val=50
+        if count>50:
+            for i in range(val):  
+                if count % val==i:
+                    print(f"analysing image {i}")
+                    fname = "./img_analysis/june23_withlargemag_right.npz"
+                    IMAGE_LIST.append(image_data)
+                    np.savez(fname,*IMAGE_LIST)
+                
+            if len(IMAGE_LIST) == val:
+                arr0 = np.zeros(CAM_IMG_SIZE)
+                for arr in IMAGE_LIST:
+                    arr0 += arr
+                arr0 /= val
+                #print(np.max(arr0))
+                mean_arr = np.mean(arr0[arr0>1800])
+                print(mean_arr)   
+                arr0=np.zeros(CAM_IMG_SIZE)
+                exit()
+                    
+        
+
         
 
 image_queue = queue.Queue()
@@ -36,10 +64,12 @@ def image_to_bytes(image):
     buf = image.to_bytearray()[0]
     width, height = image.get_size()
     data = np.frombuffer(buf, dtype='<u2')   # uint16 little‐endian
+    
     arr = data.reshape((height, width))
     return arr
 
 def update_plot(*args):
+
     try:
         while True:  # Drain the queue
             image_data = image_queue.get_nowait()
@@ -48,15 +78,16 @@ def update_plot(*args):
             original_image.set_data(image_data)
             
             # image_transformed = ff.apply_transformations(image_data)
-            image_transformed = MAG_COMP
+            image_transformed = image_data - MAG_BASELINE
             filtered_image.set_clim(vmin=np.min(image_transformed), vmax=np.max(image_transformed))
             filtered_image.set_data(image_transformed)
-
+            
             print("updating plot")
             fig.canvas.draw_idle()
             fig.canvas.flush_events()
             
-            # save 
+            # save
+
             
 
     except queue.Empty:
@@ -66,19 +97,18 @@ def update_plot(*args):
     return True
 
 def main():
-    global original_image, filtered_image, fig, MAG_COMP, MAG_BASELINE
+    global original_image, filtered_image, fig
+    
     
     
     fig, ax = plt.subplots(1, 2, figsize=(12, 6))
     data = np.zeros(CAM_IMG_SIZE)
     
-    MAG_COMP = np.zeros(CAM_IMG_SIZE)
-    MAG_BASELINE = np.zeros(CAM_IMG_SIZE)
-    
+
     original_image = ax[0].imshow(data, cmap='gray')  # Assuming 16-bit grayscale
     ax[0].set_title("Original Image")
     ax[0].axis('off')
-    filtered_image = ax[1].imshow(MAG_COMP, cmap='gray')
+    filtered_image = ax[1].imshow(MAG_BASELINE, cmap='gray')
     ax[1].set_title("After Filters")
     ax[1].axis('off')
     plt.ion()  # Interactive mode ON
@@ -91,7 +121,7 @@ def main():
     
     cam.open_camera("29035")
     
-    cam.set_setting('exposure_ms', 100)
+    cam.set_setting('exposure_ms', 1)
     cam.set_setting('trigger_type', 'SW Trigger')
     cam.set_setting('trigger_count', 2000)
 
@@ -102,7 +132,7 @@ def main():
     
     
     print("start timer")
-    timer = fig.canvas.new_timer(interval=50)
+    timer = fig.canvas.new_timer(interval=100)
     timer.add_callback(update_plot)
     timer.start()
 
